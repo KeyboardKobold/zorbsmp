db5f420b18570f9f6c500e3a091e4cf3d1aa4cb1
KeyboardKobold <christian@bodenbender.work>
2022-06-14 20:44:09 +0200

    added gallery and styling

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="content">
-        Gallery!
-
-        <img :src=images[0].pathLong />
+        <img class="image" v-for="(image, i) in images" :src="image.url" :key="i" @click="index = i">
+        <vue-gallery-slideshow :images="images" :index="index" @close="index = null"></vue-gallery-slideshow>
     </div>
 </template>
 
 <script>
+    import VueGallerySlideshow from 'vue-gallery-slideshow';
+
     export default {
         name: "GalleryComponent",
+        components: {
+            VueGallerySlideshow
+        },
         data() {
             return {
                 images: [],
+                index: null
             };
         },
 
@@ -21,12 +26,30 @@
 
         methods: {
             importAll(r) {
-                r.keys().forEach(key => (this.images.push({ pathLong: r(key), pathShort: key })));
+                r.keys().forEach(key => (this.images.push({ url: r(key) }))); /* , pathShort: key }))); */
             },
         },
     }
 </script>
 
 <style scoped>
+
+.content {
+    margin: 8;
+}
+
+.image {
+    height: auto; 
+    width: auto; 
+    width: 300px; 
+    height: 300px;
+    margin: 8em 0 0 8em;
+    border-radius: 8px;
+    object-fit: cover;
+}
+
+.image:hover {
+  opacity: 0.3;
+}
 
 </style>
