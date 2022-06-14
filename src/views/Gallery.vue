123e4fd2f430457862dc562b37a539380c4c082a
KeyboardKobold <christian@bodenbender.work>
2022-06-14 20:08:06 +0200

    added basic resource scanning for gallery provision

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="content">
         Gallery!
+
+        <img :src=images[0].pathLong />
     </div>
 </template>
 
 <script>
     export default {
         name: "GalleryComponent",
+        data() {
+            return {
+                images: [],
+            };
+        },
+
+        mounted() {
+            this.importAll(require.context('../assets/gallery/5/', true, /\.jpg$/));
+        },
+
+        methods: {
+            importAll(r) {
+                r.keys().forEach(key => (this.images.push({ pathLong: r(key), pathShort: key })));
+            },
+        },
     }
 </script>
 
