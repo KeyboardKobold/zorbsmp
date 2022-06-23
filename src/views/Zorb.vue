2d58184e1105c398122250c02e4069e0d3d9191d
KeyboardKobold <christian@bodenbender.work>
2022-06-24 00:32:46 +0200

    added zorb view

--- src/views/Zorb.vue
+++ src/views/Zorb.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="content">
-        Zorb!
+    <div class="content d-flex d-row align-items-center">
+        <div class="content-wrapper text-center">
+            <div class="image">
+                <p>click me</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +15,28 @@
 </script>
 
 <style scoped>
+
+.content {
+    margin: 8em 0;
+}
+
+.content-wrapper {
+    width: 100%
+}
+
+.image {
+    background-image: url('../assets/harry_censored.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    max-width: 70%;
+    min-width: 50%;
+    margin: 0 auto;
+    height: 100vh;
+}
+
+.image:active {
+    background-image: url('../assets/harry.png');
+}
 
 </style>
